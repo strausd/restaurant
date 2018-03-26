07065eff26ea3c03892a0e1391d4bde638a68868
Danny Straus <strausd@me.com>
2018-03-25 21:13:28 -0500

    Animations fully working on mobile and desktop

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,23 +8,66 @@ import MapPage from './MapPage';
 import DetailPage from './DetailPage';
 
 
-export const HomePage = ({ location }) => {
-    return (
-        <div className="page-content">
-            <RestaurantsList />
+export class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-            <TransitionGroup>
-                <CSSTransition key={location.pathname} classNames="slide" timeout={250} mountOnEnter unmountOnExit>
-                    <div className={location.pathname === '/' ? '' : 'slide'}>
-                        <Switch location={location}>
-                            <Route exact path="/map" component={MapPage} />
-                            <Route exact path="/:id" component={DetailPage} />
-                        </Switch>
-                    </div>
-                </CSSTransition>
-            </TransitionGroup>
-        </div>
-    );
-};
+    componentDidMount() {
+        this.handleBodyNoScroll();
+    }
+
+    componentDidUpdate() {
+        this.handleBodyNoScroll();
+    }
+
+    handleBodyNoScroll = () => {
+        if (this.props.location.pathname === '/') {
+            document.querySelector('body').classList.remove('noscroll');
+        } else {
+            setTimeout(() => {
+                document.querySelector('body').classList.add('noscroll');
+            }, 250);
+        }
+    };
+    
+    render() {
+        return (
+            <div className="page-content">
+                <RestaurantsList />
+
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.pathname} classNames="slide" timeout={250} mountOnEnter unmountOnExit>
+                        <div className={this.props.location.pathname === '/' ? '' : 'slide'}>
+                            <Switch location={this.props.location}>
+                                <Route exact path="/map" component={MapPage} />
+                                <Route exact path="/:id" component={DetailPage} />
+                            </Switch>
+                        </div>
+                    </CSSTransition>
+                </TransitionGroup>
+            </div>
+        );
+    }
+}
+
+// export const HomePage = ({ location }) => {
+//     return (
+//         <div className="page-content">
+//             <RestaurantsList />
+
+//             <TransitionGroup>
+//                 <CSSTransition key={location.pathname} classNames="slide" timeout={250} mountOnEnter unmountOnExit>
+//                     <div className={location.pathname === '/' ? '' : 'slide'}>
+//                         <Switch location={location}>
+//                             <Route exact path="/map" component={MapPage} />
+//                             <Route exact path="/:id" component={DetailPage} />
+//                         </Switch>
+//                     </div>
+//                 </CSSTransition>
+//             </TransitionGroup>
+//         </div>
+//     );
+// };
 
 export default HomePage;
